370cfa324285a756c17b44ec1b606bcffa1ff0e0
cristian lozano <yatogami2814@gmail.com>
2023-02-02 23:15:33 -0500

    creacion del hover con css y medidas responsive

--- NEWBIE/TO-DO article-preview-component-master/index.js
+++
@@ -1,6 +0,0 @@
-const shareButton = document.querySelector("#boton-redes");
-const shareredes = document.querySelector("#redes");
-
-shareButton.addEventListener("click", () => {
-  shareredes.classList.toggle("hiden");
-});
