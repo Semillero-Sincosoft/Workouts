ebb3a6a281360b22d42ca3ab41c24f5ff40845c0
cristian lozano <yatogami2814@gmail.com>
2023-03-07 14:46:16 -0500

    creación de rama

---
+++ NEWBIE/TO_DO news-homepage-main/main.js
@@ -0,0 +1,13 @@
+const main = document
+  .createElement("main")
+  .appendChild(document.createElement("header"));
+const section = document.createElement("section");
+const nav = document.createElement("nav");
+const div = document.createElement("div");
+const button = document.createElement("button");
+const image = document.createElement("img");
+const article = document.createElement("article");
+const footer = document.createElement("footer");
+main.classList.add("main");
+
+section.classList.add("content");
