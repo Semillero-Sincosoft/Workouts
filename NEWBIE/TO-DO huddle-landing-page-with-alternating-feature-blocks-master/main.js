6fdd4a41cd440061398c75b325cc18206d91eb06
Santiago Rios <riosnelson2017@gmail.com>
2023-04-03 12:11:20 -0500

    Hundle Js Finalizado

--- NEWBIE/TO-DO huddle-landing-page-with-alternating-feature-blocks-master/main.js
+++ NEWBIE/TO-DO huddle-landing-page-with-alternating-feature-blocks-master/main.js
@@ -137,6 +137,143 @@ contentTextCardThree.append(pCardThree);
 //main
 
 //footer
+const footer = document.createElement('footer');
+contentHome.append(footer);
 
+const contentAbove = document.createElement('section');
+contentAbove.classList.add('content-above');
+footer.append(contentAbove);
+
+const h3ContentAbove = document.createElement('h3');
+h3ContentAbove.textContent = "Ready To Build Your Community?";
+contentAbove.append(h3ContentAbove);
+
+const buttonContentAbove = document.createElement('button');
+contentAbove.append(buttonContentAbove);
+
+const pContentAbove = document.createElement('p');
+pContentAbove.textContent = "Get Started For Free";
+buttonContentAbove.append(pContentAbove);
+
+
+const contentContact = document.createElement('section');
+contentContact.classList.add("content-contact");
+footer.append(contentContact);
+
+const infoContact = document.createElement('article');
+infoContact.classList.add('info-contact');
+contentContact.append(infoContact);
+
+const contentLogoFooter = document.createElement('article');
+contentLogoFooter.classList.add ('content-logo-footer');
+infoContact.append(contentLogoFooter);
+
+const logoFooter = document.createElement('img');
+logoFooter.classList.add ('logo-footer');
+logoFooter.src = "images/logo-white.svg";
+contentLogoFooter.append(logoFooter);
+
+const contactInfo = document.createElement('article');
+contactInfo.classList = 'contact-info';
+infoContact.append(contactInfo);
+
+const locationContent = document.createElement('article');
+locationContent.classList = "location";
+contactInfo.append(locationContent);
+
+const iconLocation = document.createElement('img');
+iconLocation.classList.add ("icon-location")
+iconLocation.src = "images/icon-location.svg";
+locationContent.append(iconLocation);
+
+const pLocation = document.createElement('p');
+pLocation.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+locationContent.append(pLocation);
+
+const phone = document.createElement('article');
+phone.classList.add("phone");
+contactInfo.append(phone)
+
+const imgPhone = document.createElement('img');
+imgPhone.classList.add("icon-phone");
+imgPhone.src = "images/icon-phone.svg";
+phone.append(imgPhone);
+
+const pPhone = document.createElement('p');
+pPhone.textContent = "+1-543-123-4567";
+phone.append(pPhone);
+
+const email = document.createElement('article');
+email.classList.add('email');
+contactInfo.append(email);
+
+const imgEmail = document.createElement('img');
+imgEmail.classList.add("icon-email");
+imgEmail.src = "images/icon-email.svg"
+email.append(imgEmail);
+
+const pEmail = document.createElement('p');
+pEmail.textContent = "example@huddle.com";
+email.append(pEmail);
+
+const companyInfo = document.createElement('article');
+companyInfo.classList = "company-info";
+contentContact.append(companyInfo);
+
+const column1 = document.createElement('article');
+column1.classList.add('column-1');
+companyInfo.append(column1);
+
+const aboutUs = document.createElement('p');
+aboutUs.textContent = "About Us";
+column1.append(aboutUs);
+
+const whatWeDo = document.createElement('p');
+whatWeDo.textContent = "What We Do";
+column1.append(whatWeDo);
+
+const faq = document.createElement('p');
+faq.textContent = "FAQ";
+column1.append(faq);
+
+const column2 = document.createElement('article')
+column2.classList.add("column-2");
+companyInfo.append(column2);
+
+const career = document.createElement('p');
+career.textContent = "Career";
+column2.append(career);
+
+const blog = document.createElement('p');
+blog.textContent = 'Blog';
+column2.append(blog);
+
+const pContact = document.createElement('p');
+pContact.textContent = "Contact";
+column2.append(pContact);
+
+
+const contentSocial = document.createElement('article');
+contentSocial.classList.add("content-social");
+contentContact.append(contentSocial);
+
+const icons = document.createElement('article');
+icons.classList.add("icons")
+contentSocial.append(icons);
+
+const contentIconFacebook = document.createElement('img');
+contentIconFacebook.src = "images/facebook (4) (1).svg"
+contentIconFacebook.classList.add("icon");
+icons.append(contentIconFacebook);
+
+const contentIconTwitter = document.createElement('img');
+contentIconTwitter.src = "images/gorjeo (2) (1).svg";
+contentIconTwitter.classList.add("icon");
+icons.append(contentIconTwitter);
+
+const contentIconInstagram = document.createElement('img');
+contentIconInstagram.src = "images/instagram (3) (1).svg";
+contentIconInstagram.classList.add("icon");
+icons.append(contentIconInstagram);
 
 //footer
