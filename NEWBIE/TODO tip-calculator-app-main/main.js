6b6995ee215c3666bf04dd3ffb458dcd2adef5fb
Santiago Rios <riosnelson2017@gmail.com>
2023-04-20 15:40:16 -0500

    re factorización

--- NEWBIE/TODO tip-calculator-app-main/main.js
+++ NEWBIE/TODO tip-calculator-app-main/main.js
@@ -1,17 +1,16 @@
 const intoBill = document.getElementById("invoice");
-const buttons = document.querySelectorAll(".content-options-tips button");
+const button = document.querySelectorAll(".content-options-tips button");
 const numberOfPeople = document.getElementById("num-people");
 const customTip = document.getElementById("custom");
 const resultTipAmount = document.getElementById("result-tip");
 const resultTotalAmount = document.getElementById("result-total");
 const resetButton = document.getElementById("reset-all");
-
 //Calculate Tip When Click On Tip Percentage Button
-buttons.forEach((buttons) => {
+intoBill.toLocaleString('es-CO', {style: 'currency', currency : 'COP', minimumFractionDigits:0});
+button.forEach((buttons) => {
   buttons.addEventListener("click", (e) => {
-    let tipValue = e.target.textContent;
-    tipValue = tipValue.substr(0, tipValue.length - 1);
-
+    let tipValue = e.target.textContent.replace("%", "");
+    // let tipValue = e.target.textContent.replace("%",""); re factorizado linea 11
     if (intoBill.value === "") return;
     if (numberOfPeople.value === "") numberOfPeople.value = 1;
 
@@ -20,17 +19,15 @@ buttons.forEach((buttons) => {
       parseInt(tipValue),
       parseInt(numberOfPeople.value)
     );
-
   });
 });
-
 //Calculate Tip When User Give Custom Tip Percentage Input
-customTip.addEventListener("keyup", (e) =>{
-  if(event.keyCode == 13){
-    if (intoBill.value === ""){
-      resetEverything();
+customTip.addEventListener("keyup", (e) => {
+  if (event.keyCode == 13) {
+    if (customTip.value === "") {
+      alert("Ingrese valores")
       return;
-    }else if(numberOfPeople.value === ""){
+    } else if (numberOfPeople.value === "") {
       numberOfPeople.value = 1;
     }
     calculateTip(
@@ -40,24 +37,19 @@ customTip.addEventListener("keyup", (e) =>{
     );
   }
 });
-
-
-function calculateTip(intoBill,tipPercentage,numberOfPeople){
+function calculateTip(intoBill, tipPercentage, numberOfPeople) {
   let tipAmount = (intoBill * (tipPercentage / 100)) / numberOfPeople;
-  let tip = Math.floor(tipAmount * 100) / 100;
-  tip = tip.toFixed(2);
+  tipAmount = tipAmount.toFixed(2);
 
   let totalAmount = (tipAmount * numberOfPeople + intoBill) / numberOfPeople;
   totalAmount = totalAmount.toFixed(2);
 
-  resultTipAmount.textContent = `$${tip}`;
+  resultTipAmount.textContent = `$${tipAmount}`;
   resultTotalAmount.textContent = `$${totalAmount}`;
-
 }
-
 //reset
 resetButton.addEventListener("click", resetEverything);
-function resetEverything(){
+function resetEverything() {
   resultTipAmount.textContent = "$0.00";
   resultTotalAmount.textContent = "$0.00";
   intoBill.value = "";
