f1bd46e122df10ef8196aacd4ea35eeaed9e8678
Santiago Rios <riosnelson2017@gmail.com>
2023-04-26 09:42:16 -0500

    Refactorización de opereaciones matemáticas sobre los resultados

--- NEWBIE/TODO tip-calculator-app-main/funciones.js
+++ NEWBIE/TODO tip-calculator-app-main/funciones.js
@@ -1,11 +1,7 @@
 import {resultTipAmount, resultTotalAmount} from "./variables.js";
 export const calculateTip = function (intoBill, tipPercentage, numberOfPeople) {
   let tipAmount = (intoBill * (tipPercentage / 100)) / numberOfPeople;
-  tipAmount = tipAmount.toFixed(2);
-
-  let totalAmount = (tipAmount * numberOfPeople + intoBill) / numberOfPeople;
-  totalAmount = totalAmount.toFixed(2);
-
-  resultTipAmount.textContent = `$${tipAmount}`;
-  resultTotalAmount.textContent = `$${totalAmount}`;
+  let totalAmount = (intoBill / numberOfPeople) + tipAmount;
+  resultTipAmount.textContent = `$${tipAmount.toFixed(2)}`;
+  resultTotalAmount.textContent = `$${totalAmount.toFixed(2)}`;
 }
