0d5bce9a06b04f5adf2ef73db33bbbd122332c4a
cristian lozano <yatogami2814@gmail.com>
2023-04-28 09:19:32 -0500

    intento 1 merge

--- NEWBIE/TODO tip-calculator-app-main/globalVariables.js
+++
@@ -1,7 +0,0 @@
-export const intoBill = document.getElementById("bill");
-export const customPercentage = document.getElementById("custom");
-export const tipButtons = document.querySelectorAll(".content-buttons button");
-export const numberPeople = document.getElementById("people");
-export const billTip = document.getElementById("tip-amount");
-export const resetButton = document.getElementById("reset-button");
-export const moneyPerPerson = document.getElementById("total");
