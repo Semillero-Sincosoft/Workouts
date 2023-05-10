a832d085972360322244899e0f1f3f74a22ba851
cristian lozano <yatogami2814@gmail.com>
2023-05-10 15:11:28 -0500

    terminacion del responsive y js comenzado

--- NEWBIE/TO-DO crowdfunding-product-page-main/index.js
+++ NEWBIE/TO-DO crowdfunding-product-page-main/index.js
@@ -1,16 +1,56 @@
 const navbar = document.querySelector("nav");
 const openMenu = document.getElementById("menu-button");
 const closeMenu = document.getElementById("menu-close");
-const prueba = document.getElementById("");
 
-openMenu.addEventListener("click", () => {
-  navbar.classList.add("open");
-  openMenu.style.display = "none";
-  closeMenu.style.display = "flex";
+const openCLose = () => {
+  openMenu.addEventListener("click", () => {
+    navbar.classList.add("open");
+    openMenu.style.display = "none";
+    closeMenu.style.display = "flex";
+  });
+
+  closeMenu.addEventListener("click", () => {
+    navbar.classList.remove("open");
+    openMenu.style.display = "flex";
+    closeMenu.style.display = "none";
+  });
+};
+
+openCLose();
+
+const buttonProyect = document.getElementById("button-proyect");
+const cardAlert = document.getElementById("card-alert");
+const div = document.getElementById("div");
+buttonProyect.addEventListener("click", () => {
+  cardAlert.style.display = "flex";
+  div.classList.add("xd");
 });
 
-closeMenu.addEventListener("click", () => {
-  navbar.classList.remove("open");
-  openMenu.style.display = "flex";
-  closeMenu.style.display = "none";
+const btnGotIt = document.getElementById("btn-got-it");
+
+btnGotIt.addEventListener("click", () => {
+  cardAlert.style.display = "none ";
+
+  div.style.display = "none";
+});
+
+const pbookmark = document.getElementById("p-bookmark");
+const btnBookmark = document.getElementById("btn-bookmark");
+
+// document.body.addEventListener("click", () => {
+//   pbookmark.textContent = "boomark";
+// });
+
+// btnBookmark.addEventListener("click", () => {});
+
+window.addEventListener("click", function (e) {
+  if (btnBookmark.contains(e.target)) {
+    pbookmark.textContent = "Bookmarked";
+    pbookmark.style.color = "var(--Dark-cyan)";
+    // Clicked in box
+  } else {
+    pbookmark.textContent = "Bookmark";
+    pbookmark.style.color = "#bbb";
+    // Clicked outside the box
+  }
 });
