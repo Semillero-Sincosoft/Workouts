551185e42a245090c25212d98cac5fd138dbfca3
Santiago Rios <riosnelson2017@gmail.com>
2023-05-12 01:48:23 -0500

    Implementación de funcionalidad parte 1

--- NEWBIE/TO-DO crowdfunding-product-page-main/main.js
+++ NEWBIE/TO-DO crowdfunding-product-page-main/main.js
@@ -2,13 +2,26 @@ const navContainer = document.querySelector("nav");
 const openMenu = document.getElementById("open");
 const closeMenu = document.getElementById("close");
 
+const checkSupport = document.getElementById("check-support")
+const openSupport = document.getElementById("open-support");
+const closeSupport = document.getElementById("close-support")
+
 openMenu.addEventListener("click", ()=>{
-    navContainer.classList.toggle("prueba")
+    navContainer.classList.add("nav-open")
     openMenu.style.display = "none";
     closeMenu.style.display = "flex";
 })
 closeMenu.addEventListener("click",()=>{
-    navContainer.classList.toggle("prueba");
+    navContainer.classList.remove("nav-open");
     openMenu.style.display = "flex"
     closeMenu.style.display = "none"
+})
+
+openSupport.addEventListener("click",()=>{
+    checkSupport.classList.add("popup-notification");
+    checkSupport.style.display = "flex"
+})
+closeSupport.addEventListener("click",()=>{
+    checkSupport.classList.remove("popup-notification")
+    checkSupport.style.display = "none"
 })
