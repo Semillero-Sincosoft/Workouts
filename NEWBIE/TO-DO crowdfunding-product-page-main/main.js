a5232d7eb860d20d2765d3303b7a09e1ce562a2a
Santiago Rios <riosnelson2017@gmail.com>
2023-05-17 00:21:37 -0500

    estilos de tarjetas y funcionalidad de despliegue

--- NEWBIE/TO-DO crowdfunding-product-page-main/main.js
+++ NEWBIE/TO-DO crowdfunding-product-page-main/main.js
@@ -5,6 +5,32 @@ function selectCard(container,input){
 }
 selectCard("card-1","input-card-1")
 selectCard("card-2","input-card-2")
+selectCard("card-3","input-card-3")
+
+const cardOne = document.getElementById("card-1");
+const contentAmountCardOne = document.getElementById("content-amount-card-one");
+
+const cardTwo = document.getElementById("card-2");
+const contentAmountCardTwo = document.getElementById("content-amount-card-two");
+
+const cardThree = document.getElementById("card-3")
+const contentAmountCardThree = document.getElementById("content-amount-card-three")
+
+cardOne.addEventListener("click",() => {
+  contentAmountCardOne.style.display = "flex"
+  contentAmountCardTwo.style.display = "none"
+  contentAmountCardThree.style.display = "none"
+})
+cardTwo.addEventListener("click",() =>{
+  contentAmountCardOne.style.display = "none"
+  contentAmountCardTwo.style.display = "flex"
+  contentAmountCardThree.style.display = "none"
+})
+cardThree.addEventListener("click",() => {
+  contentAmountCardOne.style.display = "none"
+  contentAmountCardTwo.style.display = "none"
+  contentAmountCardThree.style.display = "flex"
+})
 const nav = {
   element: document.getElementById("nav-content"),
   iconOpen: document.getElementById("open"),
