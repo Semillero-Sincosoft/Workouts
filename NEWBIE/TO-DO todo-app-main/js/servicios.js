1221f088be85e7391fbcd8017c2ae2c3c5e7e427
Santiago Rios <riosnelson2017@gmail.com>
2023-05-31 12:07:01 -0500

    cargar pagina sin tareas por defecto

--- NEWBIE/TO-DO todo-app-main/js/servicios.js
+++ NEWBIE/TO-DO todo-app-main/js/servicios.js
@@ -1,8 +1,4 @@
-export const tareas = [
-  { texto: "ir al banco", finalizado: true },
-  { texto: "limpiar zapatos", finalizado: false },
-  { texto: "barrer la calle", finalizado: true },
-];
+export const tareas = [];
 
 export const insertarDatosTarea = (text) => {
   const nuevaTarea = { texto: text, finalizado: false };
