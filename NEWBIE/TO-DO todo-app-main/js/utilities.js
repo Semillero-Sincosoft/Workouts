4d71242bcdb3db8b4980744d96d9b6fb1767f084
Santiago Rios <riosnelson2017@gmail.com>
2023-06-02 17:06:41 -0500

    crear la función que cree el html de la barra de botones

--- NEWBIE/TO-DO todo-app-main/js/utilities.js
+++ NEWBIE/TO-DO todo-app-main/js/utilities.js
@@ -1,11 +1,15 @@
-export const createTag = (tagName,className) => {
+export const createTag = (tagName,className,text) => {
     const element = document.createElement(tagName);
     if(className){
         element.classList.add(className)
     };
+    if(text){
+        element.textContent = text;
+    }
     return element;
 }
 export const generarIdÚnico = () =>{
     const idAleatorio = Math.random().toString(8).substring(2,6);
     return idAleatorio
   }
+//   const array = [createTag("h2","elementos-restantes","prueba de texto")]
--- NEWBIE/TO-DO todo-app-main/js/validationHomework.js
+++ NEWBIE/TO-DO todo-app-main/js/validationHomework.js
@@ -1,11 +1,34 @@
 import { crearTarea } from "./createHomeworks.js";
 import { createTag } from "./utilities.js";
 export const container = document.querySelector("main");
-const homeworksContent = createTag ("section","content-homework");
+const homeworksContent = createTag("section", "content-homework");
 homeworksContent.setAttribute("id", "content-homeworks");
+const contentListHomeworks = createTag("div", "contenedor-tareas");
+const contentFilters = createTag("div", "contenedor-filtros");
 
 export const insertarTarea = (tarea) => {
   const htmlTarea = crearTarea(tarea);
   container.append(homeworksContent);
-  homeworksContent.append(htmlTarea);
+  homeworksContent.append(contentListHomeworks, contentFilters);
+  contentListHomeworks.append(htmlTarea);
+  // homeworksContent.append(htmlTarea);
 };
+const filters = [
+  "items left",
+  ["All", "Active", "Completed"],
+  "Clear Completed",
+];
+filters.map((filter,index) => {
+  const element =  createTag("div","content-filter-" + index);
+  let textOfElement 
+  if(index !== 1){
+    textOfElement = createTag("p","",filter)
+    element.append(textOfElement)
+  }else{
+    const itemAll = createTag("p","",filter[0])
+    const itemActive = createTag("p","",filter[1])
+    const itemCompleted = createTag("p","",filter[2])
+    element.append(itemAll,itemActive,itemCompleted)
+  }
+  contentFilters.append(element)
+})
