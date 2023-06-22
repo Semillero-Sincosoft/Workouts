7e792c24519db2ade4f34c36fb7412fcf0747243
Santiago Rios <riosnelson2017@gmail.com>
2023-06-22 14:59:45 -0500

    refactor archivo createHomeworks y creación de funciones creadoras de elementos para la tarea

--- NEWBIE/TO-DO todo-app-main/js/tareas/creación_tareas_/createHomeworks.js
+++ NEWBIE/TO-DO todo-app-main/js/tareas/creación_tareas_/createHomeworks.js
@@ -1,39 +1,29 @@
-import icons from "../../imgs/svg.js";
 import { createTag } from "../../utilities.js";
+import { createArticle } from "./elements.js";
+import { createInput } from "./elements.js";
+import { createButtonClose } from "./elements.js";
 import { EventChecked } from "../../functionalities/checked.js";
 import { cambiarEstadoHomeWork } from "../funcionalidad_tareas_/cambiar_estado_tarea.js";
-import { eliminarTarea } from "../../functionalities/eliminar_tarea.js";
-
 export function crearTarea(tarea, index) {
   const { texto, id } = tarea;
-  const article = createTag("article", "homework");
-  article.classList.add("no-completado");
-  article.setAttribute("id", id);
-  article.setAttribute("index", index);
+  const article = createArticle(id, index);
   const div = createTag("div");
-  const input = createTag("input", "circular-checkbox");
-  input.setAttribute("type", "checkbox");
-  input.setAttribute("id", `input-${id}`);
+  const input = createInput(id);
+  const textHomework = createTag("span", "", texto, `span-${id}`);
+  const close = createButtonClose(index);
+  
+  $(article).append(div);
+  $(div).append(input, textHomework);
 
-  const textHomework = createTag("span");
-  textHomework.setAttribute("id",`span-${id}`)
-  textHomework.textContent = texto;
-
-
-  const close = createTag("button", "icon-delete");
-  close.innerHTML = icons.cross;
-  close.addEventListener("click", () => eliminarTarea(index));
-  article.append(div);
-  div.append(input, textHomework);
-
-  article.addEventListener("mouseenter", function () {
-    article.appendChild(close);
+  $(article).on("mouseenter", function () {
+    $(article).append(close);
   });
-  article.addEventListener("mouseleave", function () {
+  $(article).on("mouseleave", function () {
     article.removeChild(close);
   });
 
-  input.addEventListener("change", () => cambiarEstadoHomeWork(tarea, input));
-  input.addEventListener("change", () => EventChecked(input, textHomework));
+  $(input).on("change", () => cambiarEstadoHomeWork(tarea, input));
+  $(input).on("change", () => EventChecked(input, textHomework));
+
   return article;
 }
---
+++ NEWBIE/TO-DO todo-app-main/js/tareas/creación_tareas_/elements.js
@@ -0,0 +1,21 @@
+import { createTag } from "../../utilities.js";
+import icons from "../../imgs/svg.js";
+import { eliminarTarea } from "../../functionalities/eliminar_tarea.js";
+export const createArticle = (id, index) => {
+  const article = createTag("article", "homework", "", id, index);
+  $(article).addClass("no-completado");
+  article.setAttribute("index", index);
+  return article;
+};
+export const createInput = (id) => {
+  const input = createTag("input", "circular-checkbox", "", `input-${id}`);
+  $(input).attr("type", "checkbox");
+  return input;
+};
+
+export const createButtonClose = (index) => {
+  const close = createTag("button", "icon-delete");
+  $(close).html(icons.cross);
+  $(close).on("click", () => eliminarTarea(index));
+  return close;
+};
