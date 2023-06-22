1000af78a251b896e4b0f247bf58decdda8da059
Santiago Rios <riosnelson2017@gmail.com>
2023-06-22 10:37:26 -0500

    Solución a problema de eliminación de tarea unitaria en filtro

--- NEWBIE/TO-DO todo-app-main/js/functionalities/eliminar_tarea.js
+++ NEWBIE/TO-DO todo-app-main/js/functionalities/eliminar_tarea.js
@@ -2,6 +2,7 @@ import { insertarTarea } from "../tareas/funcionalidad_tareas_/validationHomewor
 import { obtenerTareas } from "../tareas/creación_tareas_/contenedor_tareas.js";
 import { tareasRestantes } from "../filtros/tareas_restantes.js";
 import local from "../../module/localStorage.js";
+import { validacionEstado } from "../validacion_estado.js";
 export const eliminarTarea = function (index) {
   const tarea = obtenerTareas();
   tarea.splice(index, 1);
@@ -11,4 +12,5 @@ export const eliminarTarea = function (index) {
   tarea.forEach(insertarTarea);
   const itemsLeft = document.getElementById("elements-left");
   itemsLeft.textContent = tareasRestantes() + " items left";
+  validacionEstado()
 };
