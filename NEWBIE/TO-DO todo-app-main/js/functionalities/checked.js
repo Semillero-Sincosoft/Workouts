6c2a1ed79f2eb64efcef8c9a97d32f13b645358d
Santiago Rios <riosnelson2017@gmail.com>
2023-06-22 12:57:26 -0500

    refactor archivo checked

--- NEWBIE/TO-DO todo-app-main/js/functionalities/checked.js
+++ NEWBIE/TO-DO todo-app-main/js/functionalities/checked.js
@@ -1,11 +1,10 @@
 export const EventChecked = function (input, span) {
+  const $parentElement = $(span).parent().parent();
   if (input.checked) {
-    span.classList.add("span-checked");
-    span.parentNode.parentNode.classList.remove("no-completado");
-    span.parentNode.parentNode.classList.add("completado");
+    $(span).addClass("span-checked");
+    $parentElement.removeClass("no-completado").addClass("completado")
   } else {
-    span.classList.remove("span-checked");
-    span.parentNode.parentNode.classList.remove("completado");
-    span.parentNode.parentNode.classList.add("no-completado");
+    $(span).removeClass("span-checked")
+    $parentElement.removeClass("completado").addClass("no-completado")
   }
 };
