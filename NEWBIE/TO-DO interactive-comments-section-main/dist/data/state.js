857717a122f9b1450322528cf17393dee2890882
cristian lozano <yatogami2814@gmail.com>
2023-07-07 09:08:10 -0500

    instalacion de webpack y arreglo en los archivos

--- NEWBIE/TO-DO interactive-comments-section-main/dist/data/state.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const INCREMENT = "INCREMENT";
-const DESCREMENT = "DESCREMENT";
-exports.default = {
-    positivo: INCREMENT,
-    negativo: DESCREMENT,
-};
