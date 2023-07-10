60cd3e4f82a6d1ddb1f1427387429534d3cfeaf3
cristian lozano <yatogami2814@gmail.com>
2023-07-10 17:40:28 -0500

    IMPLEMETACION DE PORTAL

--- NEWBIE/app-comments/src/App.jsx
+++ NEWBIE/app-comments/src/App.jsx
@@ -1,11 +1,15 @@
+import { createPortal } from "react-dom";
 import { Card } from "./components/Card";
-
+import Modal from "./components/Modal";
+import { useState } from "react";
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
+      {isOpen && createPortal(<Modal />, document.body)}
       <Card />
       <Card />
-      
     </>
   );
 }
--- NEWBIE/app-comments/src/components/modal.jsx
+++ NEWBIE/app-comments/src/components/modal.jsx
@@ -1,10 +1,14 @@
-// const modal = () => {
-//   <section className="content-modal">
-//     <h2 className="title-modal">Delete comment</h2>
+const Modal = () => {
+  return (
+    <section>
+      <h2 className="title-modal">Delete comment</h2>
 
-//     <p>
-//       "Are you sure want to delete this comment? This will remove the comment
-//       and can't be undone", "txtModal"
-//     </p>
-//   </section>;
-// };
+      <p>
+        Are you sure want to delete this comment? This will remove the comment
+        and can`t be undone
+      </p>
+    </section>
+  );
+};
+
+export default Modal;
