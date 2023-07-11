4d2c3d1192fe78e323ab27f9f1c7bd99678bca05
Santiago Rios <riosnelson2017@gmail.com>
2023-07-11 18:14:34 -0500

    Finalización de maquetado

---
+++ NEWBIE/product-componets/components/TextContent.jsx
@@ -0,0 +1,17 @@
+
+const TextContent = () => {
+  return (
+    <>
+      <h4 className="flex titulo">PERFUME</h4>
+      <h1 className="flex subtitle font-family">
+        Gabrielle Essence Eau De Parfum
+      </h1>
+      <p className="flex descripcion">
+        A floral, solar and voluptuous interpretation composed by Olivier Polge,
+        Perfurmer-Creator for the House of CHANEL.
+      </p>
+    </>
+  );
+};
+
+export default TextContent;
--- NEWBIE/product-componets/src/App.jsx
+++ NEWBIE/product-componets/src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
-
+import "../src/estilos.css"
+import Card from "../components/Card"
 function App() {
 
   return (
     <>
-
+      <Card />
     </>
   )
 }
