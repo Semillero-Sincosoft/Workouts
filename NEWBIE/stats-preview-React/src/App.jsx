8b2eff856c7798da5076d71ef30520b06f563d8b
Santiago Rios <riosnelson2017@gmail.com>
2023-07-13 16:24:52 -0500

    Configuración de app

--- NEWBIE/stats-preview-React/src/App.jsx
+++ NEWBIE/stats-preview-React/src/App.jsx
@@ -1,7 +1,20 @@
 import "./estilos.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="text-component">
+          <div className="numbers">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div className="img-component"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
