da941d9b81e0a0b180829f157d503ec46e83aee7
Santiago Rios <riosnelson2017@gmail.com>
2023-07-13 16:38:54 -0500

    Finalización Maquetado

--- NEWBIE/stats-preview-React/src/App.jsx
+++ NEWBIE/stats-preview-React/src/App.jsx
@@ -1,3 +1,5 @@
+import { TextContent } from "./components/TextContent";
+import { ContentImage } from "./components/ContentImage";
 import "./estilos.css";
 
 function App() {
@@ -5,13 +7,27 @@ function App() {
     <div className="container">
       <div className="content">
         <div className="text-component">
+          <TextContent
+            classTitle={"title"}
+            textTitle={`get insights that help your business grow.`}
+            classText={"subtitle"}
+            text={
+              "Discover the benefits of data analytics and make better decisions regarding revenue, customer experenrience,and overall effciency"
+            }
+          />
           <div className="numbers">
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+              <TextContent classTitle={'num'} classText={'letter-num'} textTitle={'10k'} text={'companies'} />
+            </div>
+            <div>
+            <TextContent classTitle={'num'} classText={'letter-num'} textTitle={'314'} text={'templates'} />
+            </div>
+            <div>
+            <TextContent classTitle={'num'} classText={'letter-num'} textTitle={'12m'} text={'queries'} />
+            </div>
           </div>
         </div>
-        <div className="img-component"></div>
+          <ContentImage />
       </div>
     </div>
   );
--- NEWBIE/stats-preview-React/src/components/TextContent.jsx
+++ NEWBIE/stats-preview-React/src/components/TextContent.jsx
@@ -1,7 +1,7 @@
 export const TextContent = ({ textTitle, classTitle, text, classText }) => {
   return (
     <>
-      <h1 className={classTitle}>{textTitle} </h1>
+      <h1 className={classTitle}>{textTitle} <span></span> </h1>
       <p className={classText}> {text}</p>
     </>
   );
