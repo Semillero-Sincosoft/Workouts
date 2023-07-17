1d98cec3ab31037939e4f1805083eecd8bdb5f04
Santiago Rios <riosnelson2017@gmail.com>
2023-07-17 11:43:47 -0500

    Refactorización

---
+++ NEWBIE/faq-components/src/components/img/IconArrow.jsx
@@ -0,0 +1,9 @@
+const IconArrow = () => {
+  return (
+    <>
+        <img className="flecha" src={'./src/assets/images/icon-arrow-down.svg'} alt="" />
+    </>
+  )
+}
+
+export default IconArrow
---
+++ NEWBIE/faq-components/src/components/img/ImgIllustration.jsx
@@ -0,0 +1,10 @@
+
+const ImgIllustration = () => {
+  return (
+    <>
+      <img className="img-woman" src="./src/assets/images/illustration-woman-online-mobile.svg" alt="" />
+    </>
+  )
+}
+
+export default ImgIllustration
