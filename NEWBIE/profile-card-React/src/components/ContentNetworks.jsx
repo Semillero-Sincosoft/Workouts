64d021de7cce6285664f0b1a594dc59912e521d2
cristian lozano <yatogami2814@gmail.com>
2023-07-18 14:35:36 -0500

    correcion de nombres y eliminacion de clases

--- NEWBIE/profile-card-React/src/components/ContentNetworks.jsx
+++ NEWBIE/profile-card-React/src/components/ContentNetworks.jsx
@@ -4,14 +4,14 @@ export const ContentNetworks = () => {
   return (
     <section className="num">
       <article>
-        <h1 className="follow">{profile.SocialNetworkInformation.followers}</h1>
+        <h1>{profile.SocialNetworkInformation.followers}</h1>
         <p>Followers</p>
       </article>
-      <article className="like">
+      <article>
         <h1>{profile.SocialNetworkInformation.likes}</h1>
         <p>Likes</p>
       </article>
-      <article className="photo">
+      <article>
         <h1>{profile.SocialNetworkInformation.photo}</h1>
         <p>Photo</p>
       </article>
