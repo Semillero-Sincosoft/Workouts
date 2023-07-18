346a92cb3d07b3e23b2d2710655203013ec5c456
Didier Yamit Martinez <didierymartinez@gmail.com>
2023-07-18 09:39:18 -0500

    Data cards

--- NEWBIE/3-columns-components/src/CardContent.jsx
+++ NEWBIE/3-columns-components/src/CardContent.jsx
@@ -4,30 +4,32 @@ import { ImgSedan } from "./components/image/ImgSedan";
 import { ImgSuvs } from "./components/image/ImgSuvs";
 
 export const CardContent = () => {
+  const cardsData = [
+    {
+      colorCard: "sedan",
+      image: <ImgSedan />,
+      title: "SEDANS",
+      text: "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+    },
+    {
+      colorCard: "suvs",
+      image: <ImgSuvs />,
+      title: "SUVS",
+      text: "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+    },
+    {
+      colorCard: "luxu",
+      image: <ImgLuxury />,
+      title: "LUXURY",
+      text: "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+    },
+  ];
+
   return (
     <section className="escrito">
-      <Card
-        colorCard="sedan"
-        image={<ImgSedan />}
-        title="SEDANS"
-        text="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
-      />
-      <Card
-        colorCard="suvs"
-        image={<ImgSuvs />}
-        title={"SUVS"}
-        text={
-          "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
-        }
-      />
-      <Card
-        colorCard="luxu"
-        image={<ImgLuxury />}
-        title={"LUXURY"}
-        text={
-          "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
-        }
-      />
+      {cardsData.map((cardData) => (
+        <Card key={cardData.title} {...cardData} />
+      ))}
     </section>
   );
 };
