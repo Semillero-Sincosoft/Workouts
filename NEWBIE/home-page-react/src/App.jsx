9e27ee68c3ab552ea45d88b0abde2bce5eb0363d
cristian lozano <yatogami2814@gmail.com>
2023-07-19 15:49:42 -0500

    terminacion de componentes

--- NEWBIE/home-page-react/src/App.jsx
+++ NEWBIE/home-page-react/src/App.jsx
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-import { ContainerPages } from "./components/ContainerPages";
-=======
-import DataPageAside from "./components/Data/DataPageAside";
-import { DataPageMain } from "./components/Data/DataPageMain";
-
->>>>>>> b7e43232c33d879106e57ae5d6601230552a4feb
 import "./pages.css";
+import { ContainerPages } from "./components/ContainerPages";
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <ContainerPages />
-=======
-      <DataPageMain />
-      <DataPageAside />
->>>>>>> b7e43232c33d879106e57ae5d6601230552a4feb
     </>
   );
 }
