05050e4d78f89d2ca84226763ada6a5e46c3e237
cristian lozano <yatogami2814@gmail.com>
2023-07-21 15:28:24 -0500

    creacion de entorno

---
+++ NEWBIE/crowdfunding-React/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { PageCrowdfunding } from "./components/PageCrowdfunding";
+
+function App() {
+  return (
+    <>
+      <PageCrowdfunding />
+    </>
+  );
+}
+
+export default App;
---
+++ NEWBIE/crowdfunding-React/src/components/PageCrowdfunding.jsx
@@ -0,0 +1,6 @@
+
+export const PageCrowdfunding = () => {
+  return (
+    <div>PageCrowdfunding</div>
+  )
+}
